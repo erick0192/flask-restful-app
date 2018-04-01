67f0f2699ba286a0f6c7c56aa427247dd8413075
Erick Hernandez <erick@apicalservices.eu>
2018-04-01 19:38:00 -0400

    basic auth, more endpoints

--- app.py
+++ app.py
@@ -1,15 +1,57 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from securtiy import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'erick'
 api = Api(app)
+jwt = JWT(app, authenticate, identity)
+
+items = []
 
 
-class Student(Resource):
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be blank!")
+
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda it: it['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x : x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
-api.add_resource(Student, '/student/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5001)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5001, debug=True)
